209fbce4c00cce005ff39d69fd78705fb5c176f0
Gregori Gomes Fernandes Vieira <gregori@datamotion.com.br>
2021-05-15 15:00:42 -0300

    first

---
+++ Atividade1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Atividade1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BtnSoma = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BtnDobro = new System.Windows.Forms.Button();
+            this.BtnMultiplicar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(142, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primeiro valor:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(136, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Segundo valor:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(347, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 43);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(347, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 43);
+            this.textBox2.TabIndex = 3;
+            // 
+            // BtnSoma
+            // 
+            this.BtnSoma.Location = new System.Drawing.Point(241, 230);
+            this.BtnSoma.Name = "BtnSoma";
+            this.BtnSoma.Size = new System.Drawing.Size(100, 23);
+            this.BtnSoma.TabIndex = 4;
+            this.BtnSoma.Text = "SOMA";
+            this.BtnSoma.UseVisualStyleBackColor = true;
+            this.BtnSoma.Click += new System.EventHandler(this.BtnSoma_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(166, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 37);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "RESULTADO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(347, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 37);
+            this.label4.TabIndex = 6;
+            // 
+            // BtnDobro
+            // 
+            this.BtnDobro.Location = new System.Drawing.Point(347, 231);
+            this.BtnDobro.Name = "BtnDobro";
+            this.BtnDobro.Size = new System.Drawing.Size(100, 22);
+            this.BtnDobro.TabIndex = 7;
+            this.BtnDobro.Text = "DOBRO";
+            this.BtnDobro.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnDobro.UseVisualStyleBackColor = true;
+            this.BtnDobro.Click += new System.EventHandler(this.BtnDobro_Click);
+            // 
+            // BtnMultiplicar
+            // 
+            this.BtnMultiplicar.Location = new System.Drawing.Point(453, 231);
+            this.BtnMultiplicar.Name = "BtnMultiplicar";
+            this.BtnMultiplicar.Size = new System.Drawing.Size(100, 22);
+            this.BtnMultiplicar.TabIndex = 8;
+            this.BtnMultiplicar.Text = "MULTIPLICAR";
+            this.BtnMultiplicar.UseVisualStyleBackColor = true;
+            this.BtnMultiplicar.Click += new System.EventHandler(this.BtnMultiplicar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 407);
+            this.Controls.Add(this.BtnMultiplicar);
+            this.Controls.Add(this.BtnDobro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnSoma);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BtnSoma;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button BtnDobro;
+        private System.Windows.Forms.Button BtnMultiplicar;
+    }
+}
+
---
+++ Atividade1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSoma_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2, valorSomado;
+
+            valor1 = Convert.ToInt32(textBox1.Text);
+            valor2 = Convert.ToInt32(textBox2.Text);
+
+            valorSomado = valor1 + valor2;
+
+            label4.Text = Convert.ToString(valorSomado);
+
+            MessageBox.Show("Total " + Convert.ToString(valorSomado),"Resultado da soma");
+        }
+
+        private void BtnDobro_Click(object sender, EventArgs e)
+        {
+            int valor1, valorDobrado;
+
+            valor1 = Convert.ToInt32(textBox1.Text);
+
+            valorDobrado = valor1 * 2;
+
+            label4.Text = Convert.ToString(valorDobrado);
+
+            MessageBox.Show("Total " + Convert.ToString(valorDobrado), "Resultado dobrado");
+        }
+
+        private void BtnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2, valorMultiplicado;
+
+            valor1 = Convert.ToInt32(textBox1.Text);
+            valor2 = Convert.ToInt32(textBox2.Text);
+
+            valorMultiplicado = valor1 * valor2;
+
+            label4.Text = Convert.ToString(valorMultiplicado);
+
+            MessageBox.Show("Total " + Convert.ToString(valorMultiplicado), "Resultado multiplicado");
+        }
+    }
+}
